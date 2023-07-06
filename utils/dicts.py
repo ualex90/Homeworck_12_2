85adde7465732e29f1969e0fb4ad4a48ce578c29
u_alex90 <u_alex90@mail.ru>
2023-07-06 22:40:39 +1000

    mod func get_val in dicts.py

--- utils/dicts.py
+++ utils/dicts.py
@@ -1,2 +1,12 @@
-def get_val(collection, key, default='git'):
-    pass
+def get_val(collection: dict, key, default='git'):
+    """
+    Функция возвращает значение из словаря по переданному ключу, если ключ существует.
+    В ином случае возвращается значение по-умолчанию
+    :param collection: исходный словарь
+    :param key: искомый ключ
+    :param default: значение по-умолчанию
+    :return: значение из словаря по ключу или по-умолчанию
+    """
+    val = collection.get(key)
+    value = val if val else default
+    return value
