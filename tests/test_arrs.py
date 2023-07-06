01f5bdb505c123aed754519d2693135b7058d29f
u_alex90 <u_alex90@mail.ru>
2023-07-06 22:08:14 +1000

    100% test coverage for unittest

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -26,3 +26,6 @@ class TestArrs(unittest.TestCase):
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(arrs.my_slice([1, 2, 3], -2), [2, 3])
+        self.assertEqual(arrs.my_slice([1, 2, 3], -5), [1, 2, 3])
+        self.assertEqual(arrs.my_slice([], 0), [])
