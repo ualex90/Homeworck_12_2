fa1f3ba6bdac0bda9ea92c0765711f1c84697cdf
u_alex90 <u_alex90@mail.ru>
2023-07-06 22:19:00 +1000

    unittest code commented out in test_arrs.py

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,31 +1,31 @@
-# from utils import arrs
-#
-#
-# def test_get():
-#     assert arrs.get([1, 2, 3], 1, "test") == 2
-#     assert arrs.get([], 0, "test") == "test"
-#
-#
-# def test_slice():
-#     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-#     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
-#     assert arrs.my_slice([1, 2, 3], -2) == [2, 3]
-#     assert arrs.my_slice([1, 2, 3], -5) == [1, 2, 3]
-#     assert arrs.my_slice([], 0) == []
-
-import unittest
 from utils import arrs
 
 
-class TestArrs(unittest.TestCase):
+def test_get():
+    assert arrs.get([1, 2, 3], 1, "test") == 2
+    assert arrs.get([], 0, "test") == "test"
 
-    def test_get(self):
-        self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
-        self.assertEqual(arrs.get([], 0, "test"), "test")
 
-    def test_slice(self):
-        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
-        self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
-        self.assertEqual(arrs.my_slice([1, 2, 3], -2), [2, 3])
-        self.assertEqual(arrs.my_slice([1, 2, 3], -5), [1, 2, 3])
-        self.assertEqual(arrs.my_slice([], 0), [])
+def test_slice():
+    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
+    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3], -2) == [2, 3]
+    assert arrs.my_slice([1, 2, 3], -5) == [1, 2, 3]
+    assert arrs.my_slice([], 0) == []
+
+# import unittest
+# from utils import arrs
+#
+#
+# class TestArrs(unittest.TestCase):
+#
+#     def test_get(self):
+#         self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
+#         self.assertEqual(arrs.get([], 0, "test"), "test")
+#
+#     def test_slice(self):
+#         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
+#         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+#         self.assertEqual(arrs.my_slice([1, 2, 3], -2), [2, 3])
+#         self.assertEqual(arrs.my_slice([1, 2, 3], -5), [1, 2, 3])
+#         self.assertEqual(arrs.my_slice([], 0), [])
